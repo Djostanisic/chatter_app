709a8517da2b8ad5050fd3a35e05c89a990ee860
djordjestanisic <djordje@crystalpigeon.com>
2023-06-07 15:13:23 +0200

    Initial commit

---
+++ lib/helper/helper_function.dart
@@ -0,0 +1,7 @@
+class HelperFunctions {
+
+  static String userLoggedInKey = "USER_LOGGED_IN_KEY";
+  static String userNameKey = "USER_NAME_KEY";
+  static String userEmailKey = "USER_EMAIL_KEY";
+
+}
