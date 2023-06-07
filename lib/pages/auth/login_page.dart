8a7f3822a1262413fcd80a3474f76f45cd0569f0
djordjestanisic <djordje@crystalpigeon.com>
2023-06-07 23:12:34 +0200

    Login page UI

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,3 +1,6 @@
+import 'package:chatter_app/pages/auth/register_page.dart';
+import 'package:chatter_app/widgets/widgets.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,10 +11,116 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("LoginPage"),)
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text("Chatter",
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 10),
+                const Text(
+                  "Connect with friends, share ideas, and stay updated with Chatter!",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                  textAlign: TextAlign.center,
+                ),
+                Image.asset("assets/login.png"),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Email",
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                  validator: (value) {
+                    return RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value!)
+                        ? null
+                        : "Please enter a valid email adress";
+                  },
+                ),
+                const SizedBox(height: 15),
+                TextFormField(
+                    obscureText: true,
+                    decoration: textInputDecoration.copyWith(
+                        labelText: "Password",
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                    validator: (value) {
+                      if (value!.length < 8) {
+                        return "Password must be at least 8 characters long";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        password = value;
+                      });
+                    }),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                    child: const Text(
+                      "Sign in",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    onPressed: () {
+                      login();
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text.rich(TextSpan( 
+                    text: "Don't have and acout? ",
+                    style: const TextStyle(color: Colors.black, fontSize: 14),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "Register here",
+                        style: const TextStyle(color: Colors.black,
+                        decoration: TextDecoration.underline),
+                        recognizer: TapGestureRecognizer()..onTap = () {
+                          nextScreen(context, const RegisterPage());
+                        }
+                      ),
+                    ]))
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  login() {
+    if (formKey.currentState!.validate()) {}
   }
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static String apiKey = "AIzaSyDwjuQBc10BztCKwsnZUyFDOP0X5ZR9D9Y";
   static String appId = "1:297812620930:web:41fb07599a5834ba95269b";
   static String messagingSenderId = "297812620930";
   static String projectId = "hatterappflutter-6b0cc";
+  final primaryColor = const Color(0xFFee7b64);
 }
