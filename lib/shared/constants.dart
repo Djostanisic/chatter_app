ef2ba974e00a3e41a42d7d2674a59c10a4f48fdb
djordjestanisic <djordje@crystalpigeon.com>
2023-06-07 15:13:34 +0200

    Initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String apiKey = "AIzaSyDwjuQBc10BztCKwsnZUyFDOP0X5ZR9D9Y";
+  static String appId = "1:297812620930:web:41fb07599a5834ba95269b";
+  static String messagingSenderId = "297812620930";
+  static String projectId = "hatterappflutter-6b0cc";
+}
